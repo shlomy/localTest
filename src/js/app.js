6b56d7e75816aaf28e860b593a190b79c4ed5405
shlomys <sellashlomy@gmail.com>
2018-06-24 15:06:25 +0300

    ini

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+const testModules = require('./test-module');
+require('../css/app.css');
+
+/********** Paste your code here! ************/
+
+console.log('Paste your code here!');
+console.log(testModules.hello);
